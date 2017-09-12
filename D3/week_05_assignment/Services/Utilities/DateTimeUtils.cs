817422bace7bf123d3844baee10d1bd9f3b3e8ba
skuli <skuliarnarsson@gmail.com>
2017-09-12 16:10:15 +0000

    Starting D3

---
+++ D3/week_05_assignment/Services/Utilities/DateTimeUtils.cs
@@ -0,0 +1,27 @@
+using CoursesAPI.Services.Exceptions;
+
+namespace CoursesAPI.Services.Utilities
+{
+	public class DateTimeUtils
+	{
+		public static bool IsLeapYear(int year)
+		{
+			if(year == null){
+				throw new AppObjectNotFoundException();
+			}
+			// If divisible by 4
+			if(year % 4 == 0){
+				// unless it is divisible by 100
+				if(year % 100 != 0){
+					// except when it is divisible by 400
+					if(year % 400 == 0){
+						return true;
+					}
+					return false;
+				}
+				return true;
+			}
+			return false;
+		}
+	}
+}
