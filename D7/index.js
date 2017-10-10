c436bd041bedffeb71aff320c729288a0c717c6a
Skuli Arnarsson <skulia15@ru.is>
2017-10-10 13:01:24 +0000

    Cleanup in repo

--- D7/index.js
+++ D7/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const prettier = require("prettier"); //?
+const moment = require("moment");
 const app = express();
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    res.json({message: 'Hello World'});
+    res.json({message: 'Home Page, nothing to display'});
 });
     
 // GET /api/companies
@@ -29,9 +30,13 @@ app.post('/api/companies', (req, res) => {
 //  GET /api/companies/{id}
 // Returns a given company by id
 app.get('/api/companies/:id', (req, res) => {
-    var result = companies.find(c => c.id === req.body.id);
-    console.log(result);
-    res.json(result);
+    let result = companies.filter(c => c.id == req.params.id);
+    if(result.length){
+        res.status(200).json(result);
+    }
+    else{
+        res.status(404).send('Company Not Found');
+    }
 });
 
 //   GET /api/users/
@@ -56,14 +61,46 @@ app.post('/api/users/', (req, res) => {
 // information about what company it was added to, and when it was created. It should
 // be possible to filter the list by adding a "?company={id}" to the query.
 app.get('/api/users/:id/punches', (req, res) => {
-    res.json({message: 'Returns a list of all punches registered for the given user'});
+    let user = users.filter(c => c.id == req.params.id);
+    // Find the user
+    if(!user){
+        res.status(404).send('User not found');
+    }
+    // Check if user has any punches
+    else if(!user[0].punches.length){
+        res.json({message: 'User has no punches to roll with'});
+    }
+    // If company is provided
+    if(req.query.company){
+        let allPunches = user[0].punches;
+        let companyPunches = allPunches.filter(p => p.companyId == req.query.company); 
+        if(companyPunches.length){
+            res.json(companyPunches);
+        }
+        else{
+            res.json({message: 'User has no punches from the provided company'});
+        }
+    }
+    else{
+        // Return all punches from the user
+        res.json(user[0].punches);
+    }
 });
 
 // POST /api/users/{id}/punches
 // Adds a new punch to the user account. The only information needed is the id of the
 // company
-app.get('/api/users/:id/punches', (req, res) => {
-    res.json({message: 'Adds a new punch to the user account'});
+app.post('/api/users/:id/punches', (req, res) => {
+    let user = users.filter(c => c.id == req.params.id);
+    // Find the user
+    if(!user){
+        res.status(404).send('User not found');
+    }
+    let newPunch = {};
+    newPunch.companyId = req.body.companyId;
+    newPunch.date = moment().format('LL');
+    user[0].punches.push(newPunch);
+    res.status(201).send('Punch Added!');
 });
 
 app.listen(3000, () => console.log('listening on port 3000'));
@@ -73,17 +110,48 @@ let users = [
     {
         "id": 0,
         "name": "John Johnson",
-        "email": "john@gmail.com"
+        "email": "john@gmail.com",
+        "punches": [
+            {
+                "companyId": 1,
+                "date": moment().subtract(10, 'days').format('LL')
+            },
+            {
+                "companyId": 1,
+                "date": moment().subtract(115, 'days').format('LL')
+            },
+            {
+                "companyId": 2, 
+                "date": moment().subtract(10, 'years').format('LL')
+            }
+        ]
+
     },
     {
         "id": 1,
         "name": "Jim Bob",
-        "email": "jim@gmail.com"
+        "email": "jim@gmail.com",
+        "punches": [
+            {
+                "companyId": 1,
+                "date": moment().subtract(5, 'days').format('LL')
+            },
+            {
+                "companyId": 0,
+                "date": moment().subtract(70, 'days').format('LL')
+            }
+        ]
     },
     {
         "id": 2,
         "name": "Bobba Fett",
-        "email": "bobba@gmail.com"
+        "email": "bobba@gmail.com",
+        "punches": [
+            {
+                "companyId": 0,
+                "date": moment().subtract(2, 'days').format('LL')
+            }
+        ]
     }
 ];
 
