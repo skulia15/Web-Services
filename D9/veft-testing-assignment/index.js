ded1e8e5b4516b0d69f7da4a32e43b4b8206ab30
Smari Bjorn <smarig15@ru.is>
2017-10-24 12:06:09 +0000

    mock database and snapshots

--- D9/veft-testing-assignment/index.js
+++ D9/veft-testing-assignment/index.js
@@ -1,41 +1,13 @@
-import express from 'express';
+import app from './app';
 import mongoose, {Schema} from 'mongoose';
-import errorFunction from './errorFunction';
-import {add} from './add';
 
-/* DO NOT REFACTOR THIS CODE */
-
-export const throws = n => {
-  errorFunction();
-  return n;
-};
-
-export const loop = n => {
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    sum += add(n, n - 1);
-  }
-  return sum;
-};
-/* DO NOT REFACTOR THIS CODE */
-
-/* SERVER CODE TO REFACTOR */
 mongoose.Promise = global.Promise;
-export const employeeSchema = Schema({
-  name: String,
-  jobTitles: {type: [String]},
-});
 
 mongoose
   .connect('mongodb://skulia15:skulia15@ds125365.mlab.com:25365/veft-testing', {
     useMongoClient: true,
   })
   .then(db => {
-    const Employee = db.model('Employee', employeeSchema);
-    const app = express();
-    app.get('/', (req, res) => {
-      Employee.find({}).exec((err, data) => res.json({data}));
-    });
-    app.listen(3000, () => console.log('Server running on port 3000'));
+    const server = app(db);
+    server.listen(3000, () => console.log('Server running on port 3000'));
   });
-/* SERVER CODE TO REFACTOR */
